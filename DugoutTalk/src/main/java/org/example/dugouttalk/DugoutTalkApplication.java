9eab68c7042049f73c8bed611b661b4b72c83080
GodMinseok <mseok830@naver.com>
2025-02-06 18:20:43 +0900

    DugoutTalk_Project start

---
+++ DugoutTalk/src/main/java/org/example/dugouttalk/DugoutTalkApplication.java
@@ -0,0 +1,13 @@
+package org.example.dugouttalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DugoutTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DugoutTalkApplication.class, args);
+    }
+
+}
---
+++ DugoutTalk/src/test/java/org/example/dugouttalk/DugoutTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dugouttalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DugoutTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
