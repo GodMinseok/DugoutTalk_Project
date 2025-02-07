fb59170e7126274b03f9947a6a7a8f6e1e9695b0
GodMinseok <mseok830@naver.com>
2025-02-07 21:07:52 +0900

    회원가입 시 중복 체크를 위한 UserRepository 작성

--- DugoutTalk/src/main/java/org/example/dugouttalk/repository/UserRepository.java
+++ DugoutTalk/src/main/java/org/example/dugouttalk/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package org.example.dugouttalk.repository;
 
+import org.example.dugouttalk.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
-
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUserId(String userId);
+    User findByPassword(String password);
+    User findByEmail(String email);
+    User findByNickname(String nickname);
 }
