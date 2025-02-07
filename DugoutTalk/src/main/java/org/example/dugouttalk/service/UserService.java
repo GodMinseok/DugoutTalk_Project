2ff5bc4327968c39f88ee8ee8455db6af6475962
GodMinseok <mseok830@naver.com>
2025-02-07 20:47:23 +0900

    회원가입시 필요한 UserService 생성

---
+++ DugoutTalk/src/main/java/org/example/dugouttalk/service/UserService.java
@@ -0,0 +1,11 @@
+package org.example.dugouttalk.service;
+
+import org.example.dugouttalk.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+}
